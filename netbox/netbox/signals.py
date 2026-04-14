8e4588bbca32c9443aee3d5044395c85f2cabb74
Arthur <worldnomad@gmail.com>
2026-04-14 16:09:53 -0700

    update to use post_save signal

--- netbox/netbox/signals.py
+++ netbox/netbox/signals.py
@@ -3,9 +3,3 @@ from django.dispatch import Signal
 # Signals that a model has completed its clean() method
 post_clean = Signal()
 
-# Sent after objects of a given model are created via raw save.
-# Expected call signature: post_raw_create.send(sender=MyModel, pks=[...])
-# Provides: pks (list) - PKs of the newly created objects.
-# Callers must ensure all related objects (e.g. M2M, dependent rows) are in place
-# before sending, as receivers may query related data to perform post-create work.
-post_raw_create = Signal()
