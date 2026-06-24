f76b3671cc4034d044d89467368c8bb1d9fddcd3
Arthur <worldnomad@gmail.com>
2026-06-24 13:44:33 -0700

    fixes

--- netbox/dcim/tests/test_filtersets.py
+++ netbox/dcim/tests/test_filtersets.py
@@ -8268,7 +8268,9 @@ class CoolingSourceTestCase(TestCase, ChangeLoggedFilterSetTests):
                 description='foobar3'
             ),
         )
-        CoolingSource.objects.bulk_create(cooling_sources)
+        # Use save() rather than bulk_create() so that the normalized _abs_*_temperature fields are populated
+        for cooling_source in cooling_sources:
+            cooling_source.save()
 
     def test_q(self):
         params = {'q': 'foobar1'}
@@ -8330,6 +8332,8 @@ class CoolingSourceTestCase(TestCase, ChangeLoggedFilterSetTests):
     def test_location(self):
         locations = Location.objects.all()[:2]
         params = {'location_id': [locations[0].pk, locations[1].pk]}
+        self.assertEqual(self.filterset(params, self.queryset).qs.count(), 2)
+        params = {'location': [locations[0].slug, locations[1].slug]}
         self.assertEqual(self.filterset(params, self.queryset).qs.count(), 2)
 
 
